d27ff5056bec87df6a439041e38f077fafcec6db
Nathan Adams <adamsna@datanethost.net>
2013-01-24 22:28:08 -0600

    Adding first simple test (just takes in a Base32 key and outputs the OTP)

---
+++ otpnettest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTPNet;
+
+namespace otpnettest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TOTP t = new TOTP(args[0]);
+            Console.WriteLine("Your OTP = " + t.now().ToString("D6"));
+        }
+    }
+}
